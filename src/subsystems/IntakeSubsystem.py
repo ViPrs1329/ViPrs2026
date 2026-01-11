25aa32ff458862307b10d25d8c6ac17bdd3ba626
Noah Aldridge <naldridge28@stlprioryschool.org>
2026-01-11 14:48:50 -0600

    updated file but help needed

--- src/subsystems/IntakeSubsystem.py
+++ src/subsystems/IntakeSubsystem.py
@@ -1,5 +1,6 @@
-from commands2 import Subsystem
+from commands2 import Subsystem, Command, InstantCommand
 import math
+from wpimath import units
 from phoenix6 import hardware, controls, configs, StatusCode
 from constants import Intake
 
@@ -9,3 +10,22 @@ class IntakeSubsystem(Subsystem):
 
         intakeMotor: hardware.TalonFX = hardware.TalonFX(Intake.Consts.intakeCANId)
         armMotor: hardware.TalonFX = hardware.TalonFX(Intake.Consts.armCANId)
+        intakeMotorSpeed: controls.VelocityVoltage = controls.VelocityVoltage(0).with_enable_foc(True)
+        armMotorPositioning: controls.PositionVoltage = controls.PositionVoltage(0).with_enable_foc(True)
+        intakeMotor.set_control(intakeMotorSpeed)
+        armMotor.set_control(armMotorPositioning)
+
+        self.armDown: Command = InstantCommand(
+            lambda:(armMotorPositioning.position = -0.25)
+        )
+
+        self.armUp: Command = InstantCommand(
+            lambda:(armMotorPositioning.position = 0.25)
+        )
+
+        self.intakeFuel: Command = InstantCommand(
+            lambda:(intakeMotorSpeed.velocity = 2.0)
+        )
+
+        """help me im going insane"""
+        """how do i close parentheses"""
