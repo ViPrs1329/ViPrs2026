8d4f32b5a8e9a28df2a27356e033bbe114172f76
imartin-esc <imartin7132010@gmail.com>
2026-01-11 20:09:14 -0600

    added pid control
    there are 2 slots, one that accounts for robot weight and one that doesn't

--- src/subsystems/climbing.py
+++ src/subsystems/climbing.py
@@ -1,10 +1,10 @@
 from commands2 import Subsystem
-from phoenix6 import hardware
+from phoenix6 import hardware, configs, signals, controls
 
 from constants import Climber
 
 class ClimbingSubsystem(Subsystem):
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Subsystem responsible for managing the climbing mechanism of the robot.
 
@@ -15,5 +15,26 @@ class ClimbingSubsystem(Subsystem):
         
         super().__init__()
 
-        climbingMotor: hardware.TalonFX = hardware.TalonFX(Climber.Consts.motorId)
+        self.climbingMotor: hardware.TalonFX = hardware.TalonFX(Climber.Consts.motorId)
+
+        climberConfigs: configs.TalonFXConfiguration = configs.TalonFXConfiguration()
+        # slot0 for pulling the robot up
+        # slot1 for raising the climbing arm (there is no robot to lift so no feed forward)
+        climberConfigs.slot0.with_k_p(1).with_k_i(0).with_k_d(0).with_k_g(0).with_gravity_type(signals.spn_enums.GravityTypeValue.ELEVATOR_STATIC)
+        climberConfigs.slot1.with_k_p(1).with_k_i(0).with_k_d(0)
         
+        self.climbingMotor.setNeutralMode(signals.NeutralModeValue.BRAKE)
+
+        self.climbingMotor.configurator.apply(climberConfigs)
+
+        self.climbingMotor.set_position(0.0)
+
+        self.targetPosition: float = 0.0
+        self.currentSlot: int = 1
+
+    def switchToSlot(self, slot: int = 0):
+        self.currentSlot = slot
+
+    def periodic(self) -> None:
+        positionRequest = controls.PositionTorqueCurrentFOC(position=self.targetPosition, slot=self.currentSlot)
+        self.climbingMotor.set_control(positionRequest)
