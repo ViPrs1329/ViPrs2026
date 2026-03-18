639382cbdd5ff46a75ba1e69f1cba914702ce423
imartin-esc <imartin7132010@gmail.com>
2026-03-18 08:34:19 -0500

    changed the control type to current control

--- src/generated/tuner_constants.py
+++ src/generated/tuner_constants.py
@@ -44,7 +44,7 @@ class TunerConstants:
     _steer_closed_loop_output = swerve.ClosedLoopOutputType.VOLTAGE
     # The closed-loop output type to use for the drive motors;
     # This affects the PID/FF gains for the drive motors
-    _drive_closed_loop_output = swerve.ClosedLoopOutputType.VOLTAGE
+    _drive_closed_loop_output = swerve.ClosedLoopOutputType.TORQUE_CURRENT_FOC
 
     # The type of motor used for the drive motor
     _drive_motor_type = swerve.DriveMotorArrangement.TALON_FX_INTEGRATED
