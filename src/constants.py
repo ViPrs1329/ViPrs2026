f953da6701be295e20250f9367bbd6280c015589
Noah Aldridge <naldridge28@stlprioryschool.org>
2026-09-06 09:53:07 -0500

    got rid of the limelight

--- src/constants.py
+++ src/constants.py
@@ -4,18 +4,18 @@ from pathplannerlib.config import PIDConstants
 from math import pi
 
 
-class Limelight:
+# class Limelight:
 
-    class Consts:
-        #TODO update these names to match the limelights on the robot
-        tableNames: list[str] = ["limelight-lside", "limelight-rside"]
+#     class Consts:
+#         #TODO update these names to match the limelights on the robot
+#         tableNames: list[str] = ["limelight-lside", "limelight-rside"]
 
-        # the measured standard deviation of a single limelight at a known reference area
-        standardDeviationRef: tuple[float, float, float] = (0.05, 0.05, 0.01) # meters, meters, radians
+#         # the measured standard deviation of a single limelight at a known reference area
+#         standardDeviationRef: tuple[float, float, float] = (0.05, 0.05, 0.01) # meters, meters, radians
 
-        # the reference target area at which the standard deviation was measured
-        targetAreaRef: float = 0.05 # percentage
+#         # the reference target area at which the standard deviation was measured
+#         targetAreaRef: float = 0.05 # percentage
 
-    class States:
-        pass
+#     class States:
+#         pass
 
